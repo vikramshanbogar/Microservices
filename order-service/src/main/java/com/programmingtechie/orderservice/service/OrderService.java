aa369c8b587634f9d88b21f87d69b95beb28b3df
Vikram Shanbogar <70604290+vikramshanbogar@users.noreply.github.com>
2023-11-19 19:08:31 +0530

    Updated with API-Gateway

--- order-service/src/main/java/com/programmingtechie/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/programmingtechie/orderservice/service/OrderService.java
@@ -55,7 +55,7 @@ public class OrderService {
         } else {
             throw new IllegalArgumentException("Product is not in stock, please try again later");
         }
-    }
+     }
 
     private OrderLineItems mapToDto(OrderLineItemsDto orderLineItemsDto) {
         OrderLineItems orderLineItems = new OrderLineItems();
